18cbf4e7de0a23fc047cf3524ae6a8b2b1ba47c0
Edward Mundia <eddumundia@gmail.com>
2020-06-16 15:32:15 +0300

    worked on home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,21 +1,28 @@
 <?php
 
 namespace App\Http\Controllers;
+use Tmdb\Helper\ImageHelper;
+use Tmdb\Repository\PeopleRepository;
 
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
+    
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(PeopleRepository $people, ImageHelper $helper)
     {
+        $this->people = $people;
         $this->middleware('auth');
     }
-
+    public function index(){
+        $popular = $this->people->getpopular();
+        return view('home.index', [ 'popular' => $popular ]);
+    }
     /**
      * Show the application dashboard.
      *
---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+
+@include('layouts.moviesearch')
+
+<link href='https://fonts.googleapis.com/css?family=Asset' rel='stylesheet'>
+
+</section><hr>
+<section class="hero">
+  </section>
+   
+@endsection
+
+@push('scripts')
